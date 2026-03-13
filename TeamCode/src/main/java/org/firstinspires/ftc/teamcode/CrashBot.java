47b8cecae25154dc342b6b17916c43e9f3ce84b9
MuffinHeadHD <muffinheadhd@gmail.com>
2026-03-13 07:59:48 -0400

    Update CrashBot.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CrashBot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CrashBot.java
@@ -13,14 +13,17 @@ public class CrashBot extends OpMode {
     private DcMotor lb;
     private DcMotor rb;
     private DcMotor rf;
-    private CRServo liftt;
+    private DcMotor launch;
+    private CRServo lauPush;
+
 
     public void init() {
         lf = hardwareMap.get(DcMotor.class, "lf");
         lb = hardwareMap.get(DcMotor.class, "lb");
         rb = hardwareMap.get(DcMotor.class, "rb");
         rf = hardwareMap.get(DcMotor.class, "rf");
-        liftt = hardwareMap.get(CRServo.class, "liftt");
+        launch = hardwareMap.get(DcMotor.class, "launcher");
+        lauPush = hardwareMap.get(CRServo.class, "lauPush");
 
         lf.setDirection(DcMotor.Direction.FORWARD);
         lb.setDirection(DcMotor.Direction.FORWARD);
@@ -45,12 +48,11 @@ public class CrashBot extends OpMode {
         rb.setPower(drive - turn - crab);
         rf.setPower(drive - turn + crab);
 
-        if (gamepad1.y) {
-            liftt.setPower(1.0);
-        } else if (gamepad1.a) {
-            liftt.setPower(-1.0);
-        } else{
-            liftt.setPower(0.0);
+        if (gamepad1.a) {
+            lauPush.setPower(0.6);
+            launch.setPower(1.0);
+        } else
+            launch.setPower(0.0);
+            lauPush.setPower(0.0);
         }
     }
-}
